a44013d8f0c549232b673d0dfac7b31e9273e2cc
thomas <thomas.cosialls@gmail.com>
2022-01-11 18:31:26 +0100

    update web app react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,214 @@
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import { ethers } from "ethers";
+import myDogNFT from './utils/DogNStyleNFT.json';
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'thomaskozialsky';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x6932bd6cfe89e39dcfb64ba785724c8610ffa55b";
+
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [nftMinted, setNftMinted] = useState(null)
+  const [loading, setLoading] = useState(null)
+  
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4"; 
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0 && chainId == rinkebyChainId) {
+
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+      setupEventListener()
+      getNumberNFTMinted()
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
+
+      if(chainId == rinkebyChainId)
+      {
+        /*
+        * Boom! This should print out public address once we authorize Metamask.
+        */
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]); 
+
+        getNumberNFTMinted()
+
+        setupEventListener() 
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getNumberNFTMinted = async () => {
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myDogNFT.abi, signer);
+
+        let count = await connectedContract.getTotalNFTMinted();
+
+        setNftMinted(count.toNumber())
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const askContractToMintNft = async () => {
+
+    setLoading(true)
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myDogNFT.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeDogNStyleNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+        setLoading(false)
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+        setLoading(false)
+
+      }
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myDogNFT.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewDogNFTTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already conencted :).
+  */
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Dog n' style</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Unique, sweet and stylish Dogs to collect.<br/>
+            Admire them <a href={`https://testnets.opensea.io/collection/dog-n-style-sg3st5jfkl`}>here</a>
           </p>
-          {renderNotConnectedContainer()}
+          { nftMinted != null && (<p className="sub-text">We have <u>{nftMinted} Dog NFTs minted</u> so far.</p>)}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+            ) : !loading ? 
+              (
+                <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+                  Mint NFT
+                </button>
+              )  : (<p className="sub-text">Loading...</p>)
+          }
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -33,7 +217,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
